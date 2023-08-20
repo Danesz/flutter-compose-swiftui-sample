159820fd2ff5ee4f8b6eb888b968fb2271148442
Danesz <d4n3sz@gmail.com>
2023-08-21 00:11:03 +0200

    added Compose compatibility

--- android/app/src/main/kotlin/com/danieldallos/compose_and_swiftui_sample/MainActivity.kt
+++ android/app/src/main/kotlin/com/danieldallos/compose_and_swiftui_sample/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.danieldallos.compose_and_swiftui_sample
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
